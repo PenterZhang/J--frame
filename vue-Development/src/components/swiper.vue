f2bb998d531cfeb5575acbc729efdac1865bcc58
PenterZhang <z1992752625@outlook.com>
2020-12-31 18:08:23 +0800

    swiper

--- vue-Development/src/components/swiper.vue
+++ vue-Development/src/components/swiper.vue
@@ -1,15 +1,81 @@
 <template>
-    <div class="swiper">
-        
+    <div class="swiper oh post-r">
+        <div class="swiper-image oh" :style="{'width': width + '%'}">
+            <div class="image-list fl" :style="{'width': 100 / bannerList.length +'%'}" v-for="item in bannerList" :key="item">
+                <img class="show" :src="item" alt="">
+            </div>
+        </div>
+        <div class="round flex post-a" :style="roundStyle.fath" v-if="roundBtn">
+            <span class="show" :class="[ roundMargin ? 'auto' :'' ]" v-for="(item,index) in bannerList" :style="round(roundStyle.child,roundStyle.margin,index)"></span>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    props: {
+        parameter: Object
+    },
+    data(){
+        return{
+            bannerList:[],
+            selectBtn: false,
+            roundBtn: false,
+            width: 0,
+            selectStyle:'',
+            roundStyle:'',
+            roundMargin: false,
+        }
+    },
+    methods:{
+        swiper(data){
+            this.bannerList = data.bannerList;
+            this.roundBtn = data.round.show;
+            this.width = data.bannerList.length * 100;
+            switch(data.leftAndRight.show){
+                case true:
+                    this.selectBtn = true;
+                    if(data.leftAndRight.style){
+                        this.selectStyle = data.leftAndRight.style;
+                    }else{
+                        this.selectStyle = data.leftAndRight.position;
+                    }
+                break;
+                default:
+                    this.selectBtn = false;
+                    this.selectStyle = false;
+            };
+            switch(data.round.show){
+                case true:
+                    this.roundBtn = true;
+                    if(data.round.style){
+                        this.roundStyle = data.round.style;
+                    }else{
+                        this.roundStyle = data.round.position;
+                    }
+                break;
+                default:
+                    this.roundBtn = false;
+                    this.roundStyle = false;
+            };
 
+            console.log(this.selectStyle,this.roundStyle);
+
+        },
+        round(style,margin,index){
+            if(index == 1){
+                return style + 'margin:0px ' + margin + 'px'
+            }else{
+                return style
+            }
+        }
+    },
+    created(){
+        this.swiper(this.parameter)
+    }
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
--- vue-Development/src/pages/Home.vue
+++ vue-Development/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="index">
 
-        <Swiper><!-- 轮播图 --></Swiper>
+        <Swiper :parameter="swiper"><!-- 轮播图 --></Swiper>
     
     </div>
 </template>
@@ -18,7 +18,58 @@
         },
         data(){
             return{
+                swiper:{
+                    /*
+                    
+                        1. 图片
+                        2. 是否显示左右切换按钮
+                            ①、 如果显示，位置在哪里？ 顶部？底部？还是哪里？ 这里要传style;
+                            ②、 是图片还是icon？
+                        3. 是否显示小圆点
+                            ①、 如果显示，位置哪里？ 颜色什么？也要传style
 
+                    */
+
+                    bannerList:[
+                       require('@/assets/logo.png'),
+                       require('@/assets/logo.png'),
+                       require('@/assets/logo.png')
+                    ],
+                    leftAndRight:{
+                        show: true,
+                        position: 'center', // 此处定位有三个方法： center、left、right、top,如果想自定义位置只需要把position更改为style即可,style示例： round->style
+                        // style: 'do something'
+                        icon:{
+                            isFont: false, // 区分按钮文件属性，如果是icon请看round调用示例，如果是图片，请看本示例。
+                            img: require('@/assets/images/select.png') //此处只需要放一个左图即可，会自动生成右侧图片。
+                        }
+                    },
+                    round:{
+                        show: true,
+                        // position: "center",
+                        style:{
+                            fath:`
+                                width: 22px;
+                                height: 6px;
+                                bottom:10px;
+                                right: 20px;
+                            `,
+                            child:`
+                                width:6px;
+                                height: 6px;
+                                background:black;
+                                color:white;
+                                border-radius: 50px;
+                            `,
+                            // margin: 2 //margin不写的话 默认为auto
+                        },
+                        icon:{
+                            isFont: true,
+                            img: 'icon-left' //传全局icon类名！注意icon引用格式，如果需要两个类名则两个类名都要写，如下所示：
+                            // img: 'iconfont icon-left'
+                        }
+                    }
+                }
             }
         },
         methods:{
